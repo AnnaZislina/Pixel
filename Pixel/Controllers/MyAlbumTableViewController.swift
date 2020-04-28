ee171cf0ff6f5c7b85c48ffa1d4229b6e510e5fa
Anna Zislina <annzislina@gmail.com>
2020-04-28 19:53:10 +0300

    Add functionality to MyAlbumTableVC

--- Pixel/Controllers/MyAlbumTableViewController.swift
+++ Pixel/Controllers/MyAlbumTableViewController.swift
@@ -19,6 +19,7 @@ class MyAlbumTableViewController: UIViewController {
     @IBOutlet weak var myAlbumTableView: UITableView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
+    var images: [Image] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,75 +28,38 @@ class MyAlbumTableViewController: UIViewController {
         myAlbumTableView.dataSource = self
         myAlbumTableView.rowHeight = UITableView.automaticDimension
         myAlbumTableView.estimatedRowHeight = 400
-          
-    }
-            
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         
-        DispatchQueue.main.async {
-            self.myAlbumTableView.reloadData()
+        if let savedImages = Image.loadImages() {
+            images = savedImages
+        } else {
+            images = Image.loadSampleImages()
         }
     }
-    
+            
     func presentAlert(title: String, message: String) {
-        
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
     
-
     // MARK: - Table view data source
 
-
-    
-    /*
     // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    */
-
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            images.remove(at: indexPath.row)
+            myAlbumTableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func cameraButtonTapped(_ sender: Any) {
+        showSourceTypeAlert()
     }
     
 }
@@ -103,19 +67,53 @@ class MyAlbumTableViewController: UIViewController {
 extension MyAlbumTableViewController: UITableViewDelegate, UITableViewDataSource {
     
      func numberOfSections(in tableView: UITableView) -> Int {
-        
         return 1
     }
 
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 10
+        return images.count
     }
 
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell: MyImageTableViewCell = self.myAlbumTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! MyImageTableViewCell
-
+        cell.myImageView.image = UIImage(data: images[indexPath.row].imageData!)
+        
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
+}
+
+extension MyAlbumTableViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func showSourceTypeAlert() {
+        let photoLibraryAction = UIAlertAction(title: "Choose a Photo", style: .default) { (action) in
+            self.showImagePickerController(sourceType: .photoLibrary)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        Alert.showAlert(style: .actionSheet, title: nil, message: nil, actions: [photoLibraryAction, cancelAction], completion: nil)
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerController.SourceType) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        imagePickerController.sourceType = sourceType
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            let image = Image(imageData: selectedImage.pngData()!)
+            images.append(image)
+            Image.saveImages(images)
+            dismiss(animated: true, completion: nil)
+            self.presentAlert(title: "Added!", message: "Image successfully added to album")
+        }
+    }
+    
 }
